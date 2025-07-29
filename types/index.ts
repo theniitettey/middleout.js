5c26ff4ee8b84dffb8d403ae884081fe6b2f2cc6
michaelperryjnr <michaelperryt97@gmail.com>
2025-07-29 20:47:34 +0000

    Update readme to reflect new changes

--- types/index.ts
+++ types/index.ts
@@ -5,6 +5,7 @@ export interface CompressionResult {
 }
 
 export interface MiddleOutConfig {
+  algorithm: "rle" | "stk" | "tnt" | "zph";
   aggressionLevel: number;
   preserveWhitespace: boolean;
   targetWeissman: number;
