92e843051ca1d1593b86e9147efd53f82752f56f
michaelperryjnr <michaelperryt97@gmail.com>
2025-07-30 01:16:55 +0000

    Update middelout compress and decompress

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
-import { middleOutCompress } from "middleOutCompress";
-import { middleOutDecompress } from "middleOutDecompress";
+import { middleOutCompress, compress } from "middleOutCompress";
+import { middleOutDecompress, decompress } from "middleOutDecompress";
 import { getWeissmanScore } from "weissman";
 import type { CompressionResult } from "../types";
 
 export {
   middleOutCompress,
+  compress,
   middleOutDecompress,
+  decompress,
   getWeissmanScore,
   CompressionResult,
 };
