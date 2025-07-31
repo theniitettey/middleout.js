37aa85f5f6d13507c4db8c3d2ea1a5db8c89ec9f
michaelperryjnr <michaelperryt97@gmail.com>
2025-07-31 10:09:16 +0000

    Update tsup config to build all files

--- tsup.config.cjs
+++ tsup.config.cjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["bin/cli.ts"],
+  entry: ["bin", "src", "types", "tests"],
   outDir: "dist",
   format: ["esm"],
   clean: true,
